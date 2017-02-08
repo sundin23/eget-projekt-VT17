e14159286d52513fceb958b16adc0e7257914be3
hamplord <ha.lyrstrand@hotmail.com>
2017-02-08 09:22:10 +0100

    yes

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
-var canvas
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d")
+var ballRadius = 7;
+//bollens startposition
+var ballX = canvas.width/2;
+var ballY = canvas.height-30;
+
+//målar ut bollen man skjuter
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+setInterval(draw, 10);
